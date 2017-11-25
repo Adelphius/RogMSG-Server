59edefb19b1c3f429bebc985996f665980dea37d
Justin D'Amour <damourjustin@gmail.com>
2017-11-25 17:27:15 -0500

    Added system output
    added system output to tcp server.

--- src/rogServer/TcpServer.java
+++ src/rogServer/TcpServer.java
@@ -10,16 +10,21 @@ import java.util.Scanner;
 public class TcpServer {
 
     public static void main (String args[]) throws IOException{
-
+    	System.out.println("starting server");
     	 ServerSocket listener = new ServerSocket(1245);
+    	 System.out.println("server started on port 1245");
          try {
              while (true) {
                  Socket socket = listener.accept();
+                 System.out.println("accepted connection");
                  try {
                      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                      	out.println("WORDS");
+                     	System.out.println("sending string");
+                     	
                  } finally {
                      socket.close();
+                     System.out.println("closing socket");
                  }
              }
          }
