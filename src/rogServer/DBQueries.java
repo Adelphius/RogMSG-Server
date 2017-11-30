e5edba7ad642cb46d269b452af12df3cad39603b
Sarah <33563645+saf04231@users.noreply.github.com>
2017-11-30 10:57:43 -0500

    User Queries implementation
    added implementation for most of the user quiries. Not all are working (such as the ones that return ArrayLists).

--- src/rogServer/DBQueries.java
+++ src/rogServer/DBQueries.java
@@ -5,6 +5,7 @@ package rogServer;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 /**
  * Class of database queries for talking to the RogMSG database.
  * NOTE: JDBC Driver mysql-connector-java-5.1.44 must be located in  C:\Program Files
@@ -29,7 +30,7 @@ public class DBQueries
 	private static Connection conn = null;
 	private static Statement stmt = null;
 	private static String sql = null;
-	private static String err = null;		// eventually set any err catching to use this & return
+	//private static String err = null;		// eventually set any err catching to use this & return
 	
 	
 	
@@ -46,8 +47,8 @@ public class DBQueries
 		int id = getGroupID(groupName);
 		System.out.println("Get Group ID Test Return: " + id);
 		
-		//int upGpReturn = updateGroup();
-		//System.out.println("Updated Group Return : " + upGpReturn);
+		int upGpReturn = updateGroup(id, "NewName");
+		System.out.println("Updated Group Return : " + upGpReturn);
 		
 		//int delGpReturn = delGroup();
 		//System.out.println("Updated Group Return : " + delGpReturn);
@@ -57,7 +58,6 @@ public class DBQueries
 	
 	
 	
-	// !!!!!! Have every method return a string
 	
 	
 	
@@ -65,6 +65,10 @@ public class DBQueries
 	
 	// DB connection & error
 	
+	/**
+	 * Open a connection to the Database. 
+	 * All connection parameters are static invariants.
+	 */
 	public static void connectDB() 
 	{
 		try 
@@ -73,7 +77,6 @@ public class DBQueries
 		} 
 		catch (SQLException e) 
 		{
-			// TODO have work with errLog handling 
 			e.printStackTrace();
 			System.out.println("Error: Could not connect to DB.");
 		}
@@ -81,7 +84,7 @@ public class DBQueries
 		System.out.println("Connected to : " + DB_URL); // test statement
 	}
 	
-	/*
+	/**
 	 * Close the DB connection
 	 */
 	public static void disconnectDB()
@@ -92,7 +95,6 @@ public class DBQueries
 		} 
 		catch (SQLException e) 
 		{
-			// TODO have work with errLog handling 
 			e.printStackTrace();
 		}
 		
@@ -101,70 +103,94 @@ public class DBQueries
 	
 	public static void getDBErrLog() {}
 	
-	private static void setDBErrLog() {}
+	//private static void setDBErrLog() {}
 	
 	
 	
 	// Group Queries 
+	/**
+	 * Adds a new group to the database.
+	 * 
+	 * @param groupName The name of the group created
+	 * @return The ID of the group just created. 
+	 */
 	public static int addGroup(String groupName) 
 	{
 		try 
 		{
 			stmt = conn.createStatement(); 
-			sql = "INSERT INTO msggroup (name) VALUES ('" + groupName + "');";		//TODO write the SQL Statement
-			int rs = stmt.executeUpdate(sql);
+			sql = "INSERT INTO msggroup (name) VALUES ('" + groupName + "');";
+			stmt.executeUpdate(sql);
 			
 			//returns groupID number
 			return getGroupID(groupName);
 		} 
 		catch (SQLException e) 
 		{
-			// TODO Have work with errLog handling eventually
 			e.printStackTrace();
 			return -1;
 		}
 	}
 	
-	public static int updateGroup() 
+	/**
+	 * Update a group's name given the groupID.
+	 * groupID's cannot be changed.
+	 * 
+	 * @param groupID The id of the group in the DB to change.
+	 * @param name The name that you wish to change the group to.
+	 * @return Exit status. Returns -1 if failed.
+	 */
+	public static int updateGroup(int groupID, String name) 
 	{
 		try 
 		{
 			stmt = conn.createStatement(); 
-			sql = "";		//TODO write the SQL Statement
+			sql = "UPDATE msggroup SET name='"+ name +" WHERE groupID=" + groupID + ";";
+			int rs = stmt.executeUpdate(sql);
+			
+			return rs;
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	/**
+	 * Delete a group from the Database given the groupID.
+	 * 
+	 * @param groupID The ID of the group to delete
+	 * @return Exit status. Returns -1 if failed.
+	 */
+	public static int delGroup(int groupID) 
+	{
+		try 
+		{
+			stmt = conn.createStatement(); 
+			sql = "DELETE FROM msggroup WHERE groupID = " + groupID + ";";
 			int rs = stmt.executeUpdate(sql);
 			return rs;
 		} 
 		catch (SQLException e) 
 		{
-			// TODO Have work with errLog handling eventually
 			e.printStackTrace();
 			return -1;
 		}
 	}
 	
-	public static int delGroup() 
-	{
-		try 
-		{
-			stmt = conn.createStatement(); 
-			sql = "";		//TODO write the SQL Statement
-			int rs = stmt.executeUpdate(sql);
-			return rs;
-		} 
-		catch (SQLException e) 
-		{
-			// TODO Have work with errLog handling eventually
-			e.printStackTrace();
-			return -1;
-		}
-	}
-	
+	/**
+	 * Get the ID of the group. 
+	 * 
+	 * @param groupName Name of the group to get the ID of. Group names are unique, so there will be no duplicates.
+	 * @return the ID of the group.
+	 */
 	private static int getGroupID(String groupName) 
 	{
 		try 
 		{
 			stmt = conn.createStatement(); 
-			sql = "SELECT groupID FROM msggroup WHERE name='" + groupName + "';";		//TODO write the SQL Statement
+			sql = "SELECT groupID FROM msggroup WHERE name='" + groupName + "';";
 			ResultSet rs = stmt.executeQuery(sql);
 			
 			// Extract data from result set
@@ -180,43 +206,300 @@ public class DBQueries
 		} 
 		catch (SQLException e) 
 		{
-			// TODO Have work with errLog handling eventually
 			e.printStackTrace();
 			return -1;
+		}
+	}
+	
+	/**
+	 * Get the name of a specific group.
+	 * 
+	 * @param groupID The ID of the group to get the name of.
+	 * @return The name of the group.
+	 */
+	public static String getGroupName(int groupID) 
+	{
+		try 
+		{
+			stmt = conn.createStatement(); 
+			sql = "SELECT name FROM msggroup WHERE groupID='" + groupID + "';";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			// Extract data from result set
+			String name = "";
+			while(rs.next())
+			{
+				//Retrieve by column name
+				name  = rs.getString("name");
+			}
+			rs.close();
+			
+			return name;
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			return null;
 		}
 	}
 	
 	
 	
 	// User Queries
-	public static void addUser() {}
 	
-	public static void updateUser() {}
+	/**
+	 * Adds a user to the database.
+	 * <p>
+	 * The email of the user will also be set as the default password. 
+	 * 
+	 * @param groupName The name of the group to add the user to. 
+	 * @param username The name of the new user.
+	 * @param email The email of the user created. Will also be set as the default password for the user.
+	 * @returns the ID of the new user created.
+	 */
+	public static int addUser(String groupName, String username, String email) 
+	{
+		try 
+		{
+			stmt = conn.createStatement(); 
+			sql = "INSERT INTO users (groupID, username, email, password)VALUES ('" 
+			+ getGroupID(groupName) + "," + username + "," + email + "," + email +"');";
+			stmt.executeUpdate(sql);
+			
+			//returns groupID number
+			return getUserID(groupName, username);
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			return -1;
+		}
+	}
 	
+	/**
+	 * Get the ID of the user in the given group. 
+	 * 
+	 * @param groupName Name of the group to get the user from.
+	 * @param username Name of the user account.
+	 * @return The ID of the user. Returns -1 if query failed. 
+	 */
+	private static int getUserID(String groupName, String username) {
+		try 
+		{
+			stmt = conn.createStatement(); 
+			sql = "SELECT groupID FROM msggroup WHERE groupID = '" + getGroupID(groupName) +"' AND username='" + username + "';";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			// Extract data from result set
+			int id = -1;
+			while(rs.next())
+			{
+				//Retrieve by column name
+				id  = rs.getInt("userID");
+			}
+			rs.close();
+			
+			return id;
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			return -1;
+		}
+	}
+
+	/**
+	 * Update a user's name.
+	 */
+	public static void updateUserName() 
+	{
+		
+	}
+	
+	/**
+	 * Update a user's email.
+	 */
+	public static void updateUserEmail() 
+	{
+		
+	}
+	
+	/**
+	 * Update a user's password.
+	 * 
+	 * @param userID ID of the user to update.
+	 * @param pass The new password for the user account.
+	 * @returns Exit status. Returns -1 if failed.
+	 */
+	public static int updateUserPass(int userID, String pass) 
+	{
+		try 
+		{
+			stmt = conn.createStatement(); 
+			sql = "UPDATE msggroup SET password='"+ pass +" WHERE userID=" + userID + ";";
+			int rs = stmt.executeUpdate(sql);
+			
+			return rs;
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	/**
+	 * Delete a user, given their ID.
+	 */
 	public static void delUser() {}
 	
-	public static void getUsers() {} // add to documentation
+	/**
+	 * Get all the users in a specific group.
+	 * 
+	 * @param groupID The ID of the group to get list of users from
+	 * @returns ArrayList of all the users in the group. List contains ID's and username's only
+	 */
+	public static ArrayList<User> getUsers(int groupID) 
+	{
+		try 
+		{
+			stmt = conn.createStatement(); 
+			sql = "SELECT userID, username FROM users WHERE groupID='" + groupID + "' AND username='"+ username +
+					"'" + pass +"';";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			// Extract data from result set
+			int id = -1;
+			while(rs.next())
+			{
+				//Retrieve by column name
+				id  = rs.getInt("userID");
+			}
+			rs.close();
+			
+			return id;
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			return -1;
+		}
+	} 
+	
+	/**
+	 * Get a specific user's ID based off of their username and groupID
+	 * 
+	 * @param groupID ID of the group the user is in.
+	 * @param username Username of the user to find. 
+	 * @returns Specified users ID. Returns -1 if query failed.
+	 */
+	public static int getUser(int groupID, String username) 
+	{
+		try 
+		{
+			stmt = conn.createStatement(); 
+			sql = "SELECT userID FROM users WHERE groupID='" + groupID + "' AND username='"+ username +"';";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			// Extract data from result set
+			int id = -1;
+			while(rs.next())
+			{
+				//Retrieve by column name
+				id  = rs.getInt("userID");
+			}
+			rs.close();
+			
+			return id;
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			return -1;
+		}
+	} 
+	
+	/**
+	 * Get the name and ID of a user who shares the login credentials given
+	 */
+	public static ArrayList<User> authenticate(int groupID, String username, String pass) 
+	{
+		try 
+		{
+			stmt = conn.createStatement(); 
+			sql = "SELECT userID, username FROM users WHERE groupID='" + groupID + "' AND username='"+ username +
+					"'" + pass +"';";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			// Extract data from result set
+			int id = -1;
+			while(rs.next())
+			{
+				//Retrieve by column name
+				id  = rs.getInt("userID");
+			}
+			rs.close();
+			
+			return id;
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			return -1;
+		}
+	} 
+	
+	
+	
+	// Messaging  Queries
+	public static void addMsg() 
+	{
+		
+	}
+	
+	public static void updateMsg() 
+	{
+		
+	}
+	
+	public static void delMsg() 
+	{
+		
+	}
+	
+	public static void getMsg() 
+	{
+		
+	} 
 	
 	
 	
 	// List Queries
-	public static void addList() {}
+	public static void addList() 
+	{
+	}
 	
 	public static void updateList() {}
 	
 	public static void delList() {}
 	
-	public static void getLists() {} // add to documentation
+	public static void getLists() 
+	{
+	} 
 	
 	
 	
 	// Poll Queries
-	public static void addPoll() {}
+	public static void addPoll() 
+	{
+	}
 	
 	public static void updatePoll() {}
 	
 	public static void delPoll() {}
 	
-	public static void getPolls() {} // add to documentation
+	public static void getPolls() 
+	{
+	} 
 	
 }
