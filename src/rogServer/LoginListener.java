70d1210143c8ba7ed12a14983ef251a82423cdd9
Sarah <33563645+saf04231@users.noreply.github.com>
2017-12-05 11:30:04 -0500

    Fixed server ending proccess.

--- src/rogServer/LoginListener.java
+++ src/rogServer/LoginListener.java
@@ -24,91 +24,95 @@ public class LoginListener implements Runnable {
 
 	@Override
 	public void run() {
-		
-		try {
-			
-			
-			System.out.println("Creating socket");
-			ServerSocket loginListener = new ServerSocket(_listenPort);
-			System.out.println("Socket Created. Waiting for connection.");
-			Socket socket = loginListener.accept();
-			
-			System.out.println("Socket connected.");
-	        BufferedReader input =
-	            new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	        
-	        String authType = input.readLine();
-	        
-	        if(authType.equals("login"))
-	        {
-		        System.out.println("Waiting for email.");
-		        String email = input.readLine();
-		        System.out.println("Waiting for password.");
-		        String password = input.readLine();
+		while(true)
+		{
+			try {
+				
+				
+				System.out.println("Creating socket");
+				ServerSocket loginListener = new ServerSocket(_listenPort);
+				System.out.println("Socket Created. Waiting for connection.");
+				Socket socket = loginListener.accept();
+				
+				System.out.println("Socket connected.");
+		        BufferedReader input =
+		            new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		        
+		        String authType = input.readLine();
 		        
-		        System.out.println("Authenticating.");
-		        User toAuth = ServerLogic.authenticate(email, password);
-		        
-		        PrintWriter responce = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-		        
-		        if(toAuth!=null) 
+		        if(authType.equals("login"))
 		        {
-			        System.out.println("Authenticated.");
-		        	responce.println("authenticated");
-		        	int newPort = 1024; //only 1024 for now will make it dynamic soon
-		        	responce.println(newPort);
-		        	responce.flush();
-		        	
-		        	ObjectOutputStream outToClient = new ObjectOutputStream(socket.getOutputStream());
-		        	
-		        	outToClient.writeObject(toAuth); 
-		        	
-		        	UserListener ul = new UserListener(toAuth, newPort);
-		        	
-		        	TcpServer.AddUser(ul);
-		        	
-		        }else {
-		        	responce.println("invalid");
-		        	System.out.println("invalid");
-		        }
-	        
-	        }else if(authType.equals("register"))
-	        {
-	        	System.out.println("Waiting for email.");
-		        String email = input.readLine();
-		        System.out.println("Waiting for username.");
-		        String name = input.readLine();
-		        System.out.println("Waiting for password.");
-		        String password = input.readLine();
+			        System.out.println("Waiting for email.");
+			        String email = input.readLine();
+			        System.out.println("Waiting for password.");
+			        String password = input.readLine();
+			        
+			        
+			        System.out.println("Authenticating.");
+			        User toAuth = ServerLogic.authenticate(email, password);
+			        
+			        PrintWriter responce = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+			        
+			        if(toAuth!=null) 
+			        {
+				        System.out.println("Authenticated.");
+			        	responce.println("authenticated");
+			        	int newPort = 1024; //only 1024 for now will make it dynamic soon
+			        	responce.println(newPort);
+			        	responce.flush();
+			        	
+			        	ObjectOutputStream outToClient = new ObjectOutputStream(socket.getOutputStream());
+			        	
+			        	outToClient.writeObject(toAuth); 
+			        	
+			        	UserListener ul = new UserListener(toAuth, newPort);
+			        	
+			        	TcpServer.AddUser(ul);
+			        	
+			        }else {
+			        	responce.println("invalid");
+			        	System.out.println("invalid");
+			        }
 		        
-		        User toAuth = ServerLogic.newUser(name, email, "nogroup");
-		        
-		        PrintWriter responce = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-		        
-		        if(toAuth!=null)
+		        }else if(authType.equals("register"))
 		        {
-		        	responce.println("authenticated");
-		        	int newPort = 1024; //only 1024 for now will make it dynamic soon
-		        	responce.println(newPort);
-		        	responce.flush();
-		        	
-		        	ObjectOutputStream outToClient = new ObjectOutputStream(socket.getOutputStream());
-		        	
-		        	outToClient.writeObject(toAuth); 
-		        	
-		        	UserListener ul = new UserListener(toAuth, newPort);
-		        	
-		        	TcpServer.AddUser(ul);
-		        	
+		        	System.out.println("Waiting for email.");
+			        String email = input.readLine();
+			        System.out.println("Waiting for username.");
+			        String name = input.readLine();
+			        System.out.println("Waiting for password.");
+			        String password = input.readLine();
+			        
+			        User toAuth = ServerLogic.newUser(name, email, "nogroup");
+			        
+			        PrintWriter responce = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+			        
+			        if(toAuth!=null)
+			        {
+			        	responce.println("authenticated");
+			        	int newPort = 1024; //only 1024 for now will make it dynamic soon
+			        	responce.println(newPort);
+			        	responce.flush();
+			        	
+			        	ObjectOutputStream outToClient = new ObjectOutputStream(socket.getOutputStream());
+			        	
+			        	outToClient.writeObject(toAuth); 
+			        	
+			        	UserListener ul = new UserListener(toAuth, newPort);
+			        	
+			        	TcpServer.AddUser(ul);
+			        	
+			        }
 		        }
-	        }
-	        
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		        socket.close();
+		        loginListener.close();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		
 
 	}
 
