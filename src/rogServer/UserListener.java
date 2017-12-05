97ed316ebbb0c1d899390ef177be6ee472eed2a2
Justin D'Amour <damourjustin@gmail.com>
2017-12-04 19:09:03 -0500

    adding thread listeners

--- src/rogServer/UserListener.java
+++ src/rogServer/UserListener.java
@@ -1,6 +1,15 @@
 package rogServer;
 
 public class UserListener implements Runnable {
+	
+	private User _connectedUser;
+	private int _port;
+	
+	public UserListener(User user, int port)
+	{
+		_connectedUser = user;
+		_port = port;
+	}
 
 	@Override
 	public void run() {
