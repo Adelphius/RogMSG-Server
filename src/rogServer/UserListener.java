aa82d54f5f923e1f4311ce29b967b6f048842572
Justin D'Amour <damourjustin@gmail.com>
2017-12-05 12:34:26 -0500

    stuff

--- src/rogServer/UserListener.java
+++ src/rogServer/UserListener.java
@@ -1,5 +1,11 @@
 package rogServer;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import rogShared.Message;
 import rogShared.User;
 
 public class UserListener implements Runnable {
@@ -16,6 +22,26 @@ public class UserListener implements Runnable {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		while(true)
+		{
+			try {
+				ServerSocket userListener = new ServerSocket(_port);
+				Socket socket = userListener.accept();
+				
+				ObjectInputStream fromClient = new ObjectInputStream(socket.getInputStream());
+				
+				Message msg = (rogShared.Message)fromClient.readObject();
+				
+				//ServerLogic.newMsg(, msg, recipients);
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 	}
 
